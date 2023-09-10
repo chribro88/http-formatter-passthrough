9d428f69efc7e5a44a03c4ac5cf36e548996a25a
chribro88 <chrisbrown@live.com.au>
2023-09-10 14:40:08 +1000

    Update PassthroughJsonValueFormatterTest.java

--- src/test/java/io/axual/connect/plugins/http/formatter/PassthroughJsonValueFormatterTest.java
+++ src/test/java/io/axual/connect/plugins/http/formatter/PassthroughJsonValueFormatterTest.java
@@ -5,6 +5,5 @@ public class PassthroughJsonValueFormatterTest {
     @Test
     public void placeholderTest() {
         // This is a placeholder test method
-        // You can leave it empty or add some comments
     }
 }
